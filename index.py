5a6293e6d378bdb93ec7fc0332ee05c560736dda
Camilo <camilomunozb203@gmail.com>
2025-06-19 21:44:23 -0500

    Nuevo proyecto iniciado, programado el index.

---
+++ index.py
@@ -0,0 +1,26 @@
+
+
+while True:
+    print("""
+        Bienvenido a la solicitud de imagenes:
+            1. Buscar imagenes.
+            2. Gestion de imagenes.
+            3. Salir
+        """)
+    try:
+        usuario = str(input("Ingresa la opcion que desees: ")).strip()
+        if not usuario:
+            print("El campo debe estar completo.")
+            break
+        elif usuario == "1":
+            print("Opcion a desarrollar.")
+        elif usuario == "2":
+            print("Opcion a desarrollar.")
+        elif usuario == "3":
+            print("Gracias por usar y solicitar tus imagenes favoritas.")
+            break
+        input("\nPresiona enter para continuar...")
+    except TypeError:
+        print("Error de digitacion, volver a intentar.")
+    except Exception as error:
+        print(f"Error en el programa : {error}.")
