5c2746f4c7282b9752443ac26a77e9675a89670d
Camilo <camilomunozb203@gmail.com>
2025-07-08 20:50:20 -0500

    Mejoras de errores.

--- funciones/gestor_imagen.py
+++ funciones/gestor_imagen.py
@@ -10,12 +10,17 @@ class GuardarImagen:
             if not ruta_imagen:
                 print("No se pueden tener los campos vacios.")
                 return
-            if os.path.exists(ruta_imagen):
-                ruta_destino = os.path.join(os.path.expanduser("~"),'Imágenes')
-                shutil.copy2(ruta_destino)
-                print(f"Archivo : {ruta_destino} se traslado exitosamente a {ruta_destino}")
-            else:
-                print("Imagen no encontrada.")
+            if not os.path.isfile(ruta_imagen):
+                print("La ruta no corresponde a un archivo válido. Asegúrate de que sea una imagen.")
+                return
+            ruta_destino = os.path.join(os.path.expanduser("~"),'POWER')
+            if not os.path.exists(ruta_destino):
+                os.makedirs(ruta_destino)
+                nombre_archivo = os.path.basename(ruta_imagen)
+                destino_completo = os.path.join(ruta_destino, nombre_archivo)
+                shutil.copy2(ruta_imagen,destino_completo)
+                print('destino real :',destino_completo)
+                print(f"Archivo : {ruta_imagen} se traslado exitosamente a {ruta_destino}")
         except Exception as error:
             print(f"Error en el programa: {error}.")
 
